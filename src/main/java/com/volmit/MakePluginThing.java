a8822b2f928f582c358249baee45f5b65ffc5ebd
cyberpwn <danielmillst@gmail.com>
2021-10-14 15:42:16 -0400

    Boom

---
+++ src/main/java/com/volmit/MakePluginThing.java
@@ -0,0 +1,15 @@
+package com.volmit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MakePluginThing extends JavaPlugin {
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
